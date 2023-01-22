15b261c22587a53c58aa7dc99e4bb47a231c6700
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-01-22 10:25:57 +0100

    Responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import ReactPaginate from 'react-paginate';
 
-// Example items, to simulate fetching from another resources.
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
-function Items({ currentItems }) {
-  return (
-    <>
-      {currentItems &&
-        currentItems.map((item) => (
-          <div>
-            <h3>Item #{item}</h3>
-          </div>
-        ))}
-    </>
-  );
-}
-
-function App({ itemsPerPage }) {
-  // Here we use item offsets; we could also use page offsets
-  // following the API or data you're working with.
-  const [itemOffset, setItemOffset] = useState(0);
-
-  // Simulate fetching items from another resources.
-  // (This could be items from props; or items loaded in a local state
-  // from an API endpoint with useEffect and useState)
-  const endOffset = itemOffset + itemsPerPage;
-  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-  const currentItems = items.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(items.length / itemsPerPage);
-
-  // Invoke when user click to request another page.
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
-    setItemOffset(newOffset);
-  };
-
-  return (
-    <>
-      <Items currentItems={currentItems} /><div id="container">run</div>
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-      />
-    
-    </>
-  );
-}
-
-// Add a <div id="container"> to your HTML to see the componend rendered.
-
-export default App;
-
-
-
-
-{/*import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import Login from './pages/login/login.tsx';
 import Sidebar from "./components/sidebar/Sidebar.tsx";
@@ -81,9 +16,9 @@ function App() {
   console.log(location);
    return (
     
-    <div className="App">*/}
+    <div className="App">
       {/*setup to show sidebar in the pages*/}
-      {/*{(location.pathname === "/users")  && (<><Sidebar/></>)}
+      {(location.pathname === "/users")  && (<><Sidebar/></>)}
       {(location.pathname === "/userdetails")  && (<><Sidebar/></>)}
       {(location.pathname === "/userfilters")  && (<><Sidebar/></>)}
 
@@ -98,4 +33,4 @@ function App() {
     </div>
   );
 }
-export default App;*/}
+export default App;
