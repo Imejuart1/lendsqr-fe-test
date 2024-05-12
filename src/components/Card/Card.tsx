d7ad2122ffba9d76383a4d090b6587806c12f6d1
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2024-05-12 18:49:51 +0100

    Responsive design

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, value, icon }) => {
   return (
     <div className="card">
-      <div className="card-icon">{icon}</div>
+      <div className="card-icon">  <img src={icon} alt="icon" /></div>
       <div className="card-info">
         <h3>{title}</h3>
         <p>{value}</p>
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import Card from '../../components/Card/Card.tsx';
 import Table from '../../components/Table/Table.tsx';
 import './Dashboard.scss';
+import UsersIcon from '../../assets/images/users.svg';
+import ActiveUsersIcon from '../../assets/images/active-users.svg';
+import UsersWithLoansIcon from '../../assets/images/users-with-loans.svg';
+import UsersWithSavingsIcon from '../../assets/images/users-with-savings.svg';
 
 const Dashboard: React.FC = () => {
   return (
     <div className="dashboard-container">
-        <h2>User Details</h2>
+      <div className='dashboard_main'>
+        <h2>Users</h2>
       <div className="dashboard-cards">
-        <Card title="Users" value="2,453" icon="👤" />
-        <Card title="Active Users" value="2,453" icon="👥" />
-        <Card title="Users with Loans" value="12,453" icon="💰" />
-        <Card title="Users with Savings" value="102,453" icon="💳" />
+      <Card title="Users" value="2,453" icon={UsersIcon} />
+        <Card title="Active Users" value="2,453" icon={ActiveUsersIcon} />
+        <Card title="Users with Loans" value="12,453" icon={UsersWithLoansIcon} />
+        <Card title="Users with Savings" value="102,453" icon={UsersWithSavingsIcon} />
       </div>
       <div className="dashboard-table">
-        <h2>User Details</h2>
+
         <Table />
+      </div>
       </div>
     </div>
   );
