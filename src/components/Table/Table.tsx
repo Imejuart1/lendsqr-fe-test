83534f1ab396f61b6ceee52aee63b81364ec6ed8
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2024-05-12 22:52:34 +0100

    table users started

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,21 +3,41 @@ import './Table.scss';
 
 const Table: React.FC = () => {
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>ORGANIZATION</th>
-          <th>USERNAME</th>
-          <th>EMAIL</th>
-          <th>PHONE NUMBER</th>
-          <th>DATE JOINED</th>
-          <th>STATUS</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* Your table rows go here */}
-      </tbody>
-    </table>
+    <div className='lendsqr-table'>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <span>ORGANIZATION</span>
+              <img src='/images/filter-results-button.svg' alt="Filter icon" />
+            </th>
+            <th>
+              USERNAME
+              <img src='/images/filter-results-button.svg' alt="Filter icon" />
+            </th>
+            <th>
+              EMAIL
+              <img src='/images/filter-results-button.svg' alt="Filter icon" />
+            </th>
+            <th>
+              PHONE NUMBER
+              <img src='/images/filter-results-button.svg' alt="Filter icon" />
+            </th>
+            <th>
+              DATE JOINED
+              <img src='/images/filter-results-button.svg' alt="Filter icon" />
+            </th>
+            <th>
+              STATUS
+              <img src='/images/filter-results-button.svg' alt="Filter icon" />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Your table rows go here */}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
